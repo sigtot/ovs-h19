b0005d5252191a169e08ad08c32b811b66aac980
sigtot <sigurdtotland@gmail.com>
2019-10-12 18:35:32 +0200

    Finish what should have been done in assignment 5

--- matlab/op2.m
+++ matlab/op2.m
@@ -3,17 +3,17 @@
 
 %% Create time
 h       = 0.01;  % sample time (s)
-N       = 50000; % number of samples
+N       = 10000; % number of samples
 t       = 0:h:h*(N-1);
 
 %% Insert true system
 %m = @(n) meme(t(n), 20, 20*(2-exp(-0.01*(t(n)-20))), 20);
 m = @(t) 15;
-beta = 0.2;
+beta = 0.3;
 k = 2;
 
-lambda_1 = 30;
-lambda_0 = 100;
+lambda_1 = 20;
+lambda_0 = 50;
 Lambda = [1; lambda_1; lambda_0];
 
 %% Define filters
@@ -25,90 +25,95 @@ Lambda = [1; lambda_1; lambda_0];
 %% Simulation MATLAB
 % Define input as a function of t
 %u       = @(n) 5*sin(2*t(n)) + 15;
-u       = @(t) 50*sin(2*t) + 20*cos(3*t) +30*sin(2.5*t + 5) + 15;
+u       = @(t) 100*sin(2*t) + 15;
 
 % Memory allocation
 x       = zeros(2, N);
 x_z     = zeros(2, 1);
 x_phi   = zeros(2, 1);
-x_phi_aux = zeros(2, 1);
-theta   = zeros(3, N);
+theta_k = zeros(1, N);
+theta_mb   = zeros(2, N);
 U       = zeros(N-1,1);
-masses  = zeros(1, N);
+masses = arrayfun(m, t);
 forget = 0.1;
 x_2 = zeros(2,N);
 x_1 = zeros(1,N);
 
-% Initial estimates
-%theta(:,1) = [k, beta/k, m(1)/k];
-theta(:, 1) = [1, 1, 1];
-masses(:,1) = m(1);
-
 % Initial P matrix
-gamma   = diag([100, 100, 100]);  
+gamma   = diag([0.01, 5, 35]);  
 
 %% Ode45
 y0 = [1; 1];
 [~, x_2] = ode45(@(t,y) superfunc(t, y, m, u, beta), t, y0);
 x_1 = 1/k * arrayfun(u, t)' + x_2(:, 1);
 
-%% Estimation loop
+%% Estimation loop for k
+% Main loop. Simulate using forward Euler (x[k+1] = x[k] + h*x_dot)
+for n = 1:N-1
+    z   = u(t(n));
+    phi = x_1(n) - x_2(n, 1);
+
+    % Calculate estimation error
+    epsilon = (z - theta_k(:, n)'*phi)/(1 + 0.01*(phi')*phi);
+
+    % Update law
+    theta_k_dot  = gamma(1)*epsilon*phi;
+    theta_k(n+1) = theta_k(n) + theta_k_dot*h;
+end
+
+%% Estimation loop for m and beta
 % Main loop. Simulate using forward Euler (x[k+1] = x[k] + h*x_dot)
 for n = 1:N-1
     % Generate z and phi by filtering known signals
-    x_z_n           = x_z + (A_f*x_z + B_f*(x_1(n) + u(t(n)) - x_2(n, 1)))*h;   % u is unfiltered 'z'
+    x_z_n           = x_z + (A_f*x_z + B_f*(u(t(n))))*h;   % u is unfiltered 'z'
     z               = C_f_1*x_z;                      % 1/Lambda * u
 
-    x_phi_aux_n     = x_phi_aux + (A_f*x_phi_aux + B_f*(x_1(n) - x_2(n, 1))) * h; % *h???
+    phi             = [(C_f_2*x_phi + D_f_2*x_2(n, 1)); % s/Lambda
+                       (C_f_3*x_phi + D_f_3*x_2(n, 1))]; % s^2/Lambda
     x_phi_n         = x_phi + (A_f*x_phi + B_f*x_2(n, 1))*h;
-    phi             = [(C_f_1*x_phi_aux + D_f_1*(x_1(n) - x_2(n, 1)));
-                       (C_f_2*x_phi + D_f_2*x_2(n, 1));
-                       (C_f_3*x_phi + D_f_3*x_2(n, 1))];
     
 
     % Calculate estimation error
-    epsilon         = (z - theta(:, n)'*phi)/(1 + 0.01*phi'*P(:, :, n)*phi);
+    epsilon         = (z - theta_mb(:, n)'*phi)/(1 + 0.01*(phi')*phi);
 
     % Update law
-    theta_dot       = gamma*epsilon*phi;
-    theta(:, n+1)   = theta(:, n) + theta_dot*h;
+    theta_dot       = gamma(2:3, 2:3)*epsilon*phi;
+    theta_mb(:, n+1)   = theta_mb(:, n) + theta_dot*h;
 
     % Set values for next iteration
     x_phi           = x_phi_n;
-    x_phi_aux       = x_phi_aux_n;
     x_z             = x_z_n;
     masses(:, n+1) = m(n+1);
 end
 
-
-% Plots
+%% Plots
 fig1 = figure(1);
 subplot(3,1,1)
-plot(t, theta(3,:) .* theta(1,:)); hold on
+plot(t, theta_mb(2,:)); hold on
 plot(t, masses(1,:)); hold off
 ylabel('m')
 title('Parameter estimates')
 legend('estimate','true value')
 grid
 subplot(3,1,2)
-plot(t, theta(2,:) .* theta(1, :)); hold on
+plot(t, theta_mb(1,:)); hold on
 plot([t(1), t(end)],[beta, beta]); hold off
 ylabel('\beta')
 legend('estimate','true value')
 grid
 subplot(3,1,3)
-plot(t, theta(1,:)); hold on
+plot(t, theta_k(:)); hold on
 plot([t(1), t(end)],[k, k]); hold off
 ylabel('k')
 grid
 legend('estimate','true value')
 xlabel('t [s]')
-cd
+
 %% Plot system
 fig2 = figure(2);
 plot(t, x_1); hold on;
 plot(t, x_2(:, 1));
 plot(t, x_2(:, 2));
-plot(t, u(t)); hold off;
+title('System states')
 legend('y1', 'y2', 'y2_{dot}')
 grid
