40932b0589fdae6ff45f52755bf53d850906b933
sigtot <sigurdtotland@gmail.com>
2019-10-21 13:49:12 +0200

    Make the convolution work with negative kernel values

--- assignment1/task2c.py
+++ assignment1/task2c.py
@@ -16,18 +16,21 @@ def convolve_im(im, kernel):
     """
     K = kernel.shape[0]
     kernel_offset = K // 2
-    im = np.pad(im, ((kernel_offset, kernel_offset), (kernel_offset, kernel_offset), (0, 0)), mode="constant")
-    for x in range(kernel_offset, im.shape[0] - kernel_offset):
-        for y in range(kernel_offset, im.shape[1] - kernel_offset):
-            for c in range(im.shape[2]):
-                im[x][y][c] = np.dot(np.reshape(im[x-kernel_offset:x+kernel_offset+1, y - kernel_offset:y+kernel_offset+1, c], (K*K))[::-1], np.reshape(kernel, (K*K)))
-    return im[kernel_offset:-kernel_offset, kernel_offset:-kernel_offset]
+    im = np.pad(im, ((kernel_offset, kernel_offset), (kernel_offset, kernel_offset), (0, 0)), mode="edge")
+    H, W, num_chans = im.shape[0], im.shape[1], im.shape[2]
+    out = im.copy()
+    for x in range(kernel_offset, H - kernel_offset):
+        for y in range(kernel_offset, W - kernel_offset):
+            for c in range(num_chans):
+                out[x][y][c] = np.max([np.min([np.dot(
+                    np.reshape(im[x-kernel_offset:x+kernel_offset+1, y-kernel_offset:y+kernel_offset+1, c], (K*K))[::-1],
+                    np.reshape(kernel, (K*K))), 255]), 0])
+    return out[kernel_offset:-kernel_offset, kernel_offset:-kernel_offset]
 
 
 if __name__ == "__main__":
     # Read image
-    impath = os.path.join("images", "lake.jpg")
-    lake_im = plt.imread(impath)
+    lake_im = plt.imread(os.path.join("images", "lake.jpg"))
 
     # Define the convolutional kernels
     h_a = np.ones((3, 3)) / 9
@@ -38,12 +41,26 @@ if __name__ == "__main__":
         [4, 16, 24, 16, 4],
         [1, 4, 6, 4, 1],
     ]) / 256
+    h_c = np.array([
+        [-1, -1, -1],
+        [-1, 8, -1],
+        [-1, -1, -1],
+    ])
+    h_d = np.array([
+        [0, -1, 0],
+        [-1, 5, -1],
+        [0, -1, 0],
+    ])
     # Convolve images
-    smoothed_im1 = convolve_im(lake_im.copy(), h_a)
+    smoothed_im1 = convolve_im(lake_im, h_a)
     smoothed_im2 = convolve_im(lake_im, h_b)
+    edge_im = convolve_im(lake_im, h_c)
+    sharpened_im = convolve_im(lake_im, h_d)
 
     save_im("convolved_im_h_a.png", smoothed_im1)
     save_im("convolved_im_h_b.png", smoothed_im2)
+    save_im("edge_detection.png", edge_im)
+    save_im("sharpened.png", sharpened_im)
 
     # DO NOT CHANGE
     assert isinstance(smoothed_im1, np.ndarray), \
