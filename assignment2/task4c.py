3b8ede6273a70746a7614cff0600ae381996aea4
sigtot <sigurdtotland@gmail.com>
2019-11-07 15:58:21 +0100

    Do task 4c

--- assignment2/task4c.py
+++ assignment2/task4c.py
@@ -3,20 +3,14 @@ import os
 import numpy as np
 import utils
 from task4b import convolve_im
+import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    # DO NOT CHANGE
-
     impath = os.path.join("images", "clown.jpg")
     im = skimage.io.imread(impath)
     im = utils.uint8_to_float(im)
     kernel = np.load("images/notch_filter.npy")
-
-    ### START YOUR CODE HERE ### (You can change anything inside this block)
-    im_filtered = im
-
-    ### END YOUR CODE HERE ###
-
+    im_filtered = convolve_im(im, kernel)
+    plt.show()
     utils.save_im("clown_filtered.png", im_filtered)
-
